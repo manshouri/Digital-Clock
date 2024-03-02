e402027ce6f5130fe557be9cbfb275bdad9e80d5
manshouri <manshouri611@gmail.com>
2024-03-02 09:14:42 -0800

    update the calculate time function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+window.addEventListener('load',calculateTime);
 function calculateTime(){
     let date = new Date();
+    let dayNumber =date.getDay();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second =date.getSeconds();
+    let ampm = hour >= 12 ? "PM" : "AM"; //consider AM and PM
+    let dayNames = ["SUN","MON","TUE","WED","THUR","FRI","SAT"];
+    // hour = hour % 12;
+    document.getElementById("day").innerHTML =dayNames[dayNumber];
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("second").innerHTML = second;
+    document.getElementById("ampm").innerHTML =ampm;
+
+    setTimeout(calculateTime,1000);
+
 }
